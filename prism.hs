6e8582150d6357e89a06a35836e9020485c2b042
Adjective-Object <mhuan13@gmail.com>
2015-04-06 17:55:25 -0400

    initial port to kmeans-vector

--- prism.hs
+++ prism.hs
@@ -1,7 +1,9 @@
 module Main where
 import System.Exit (exitWith, ExitCode(..))
 import Data.List (transpose)
-import Data.KMeans (kmeans)
+import qualified Data.Vector.Unboxed as V
+import qualified Data.Vector as DV
+import Math.KMeans
 import Numeric (showHex)
 import Codec.Picture (decodeImage,
                         Image,
@@ -11,37 +13,53 @@ import Codec.Picture.Types(pixelFold, convertImage, ColorSpaceConvertible)
 import System.IO (stdin)
 import Data.ByteString (hGetContents)
 
+-- quick helper because V.cons takes too long to type out
+(<:>) = V.cons
 
 -- Folds an image into a list of doubles so it can be consumed by kheap
-collapseToDoubleList :: [[Double]] -> Int -> Int -> PixelRGB8 -> [[Double]]
-collapseToDoubleList lst x y (PixelRGB8 r g b) =
-    let words = [r, g, b]
-        in lst ++ [map (\ word -> fromIntegral word :: Double) words]
+collapseToVectors ::
+    [V.Vector Double] -> Int -> Int -> PixelRGB8 -> [V.Vector Double]
+collapseToVectors lst x y (PixelRGB8 r g b) =
+    let words = r <:> ( g <:> ( b <:> V.empty))
+        in lst ++ [V.map (\ word -> fromIntegral word :: Double) words]
 
-convertImageToDoubleList :: DynamicImage -> [[Double]]
-convertImageToDoubleList img =
-    let c img = pixelFold collapseToDoubleList []  img
+convertImageToVectors :: DynamicImage -> [V.Vector Double]
+convertImageToVectors img =
+    let c img = pixelFold collapseToVectors []  img
         ci = convertImage
     in case img of
         ImageRGB8   i   -> c i
         ImageYCbCr8 i   -> c (ci i :: Image PixelRGB8)
-        _ -> [[0.0]]
+        _ -> [V.singleton 0.0]
 
+-------------------------------------------
+-- Calling kmeans and parsing the output --
+-------------------------------------------
 
--- Build a set of terminal colours using kmeans from Data.KMeans
--- returning colours in RGB format
-buildTerminalColours :: [[Double]] -> [[Double]]
-buildTerminalColours pixels = 
+-- | takes a cluster of vectors of doubles and gets the average value
+averageCluster :: Cluster(V.Vector Double) -> V.Vector Double
+averageCluster cluster =
+    let vectors :: [V.Vector Double]
+        vectors = elements cluster
+        len = fromIntegral $ length vectors :: Double
+        sumTuple2 = (\ (a, b) -> a + b )
+        sumVectors = foldl1 (\ v1 v2 -> V.map sumTuple2 (V.zip v1 v2)) vectors
+    in V.map (\a -> a/len) sumVectors
+
+-- | Builds 'terminal ready' colours from a vector of doubles
+buildTerminalColours :: [V.Vector Double] -> [V.Vector Double]
+buildTerminalColours pixels =
     let maxTries = 100
-        blocks = kmeans 6 pixels
-        colourChannels = map transpose blocks
-    in map (\ channels -> map avg channels ) colourChannels
+        clusters = kmeans id euclidSq 6 pixels
+        colourChannels = DV.map averageCluster clusters
+    in DV.toList colourChannels
 
-avg :: [Double] -> Double
-avg a = (sum a) / (fromIntegral (length a) :: Double)
+-------------------------------------------
+-- Helper functions for input and output --
+-------------------------------------------
 
-
--- Helper functions for input and output
+-- | gets the string name of a colour space, Used in reporting image format
+-- from Codec.Picture.DynamicImage
 getColourSpaceName :: DynamicImage -> String
 getColourSpaceName img = case img of
     ImageY8 _       -> "ImageY8"
@@ -65,19 +83,25 @@ rgbToHexCode channels = foldl (++) "#" hexes
                                     then "0" ++ x
                                     else x) shortHexes
 
-
 main :: IO()
 main = do
     putStrLn "reading ImageRGB8e"
     imstream <- hGetContents stdin
-    putStrLn "reading complete, passing to kmeans"
     either failure success (decodeImage imstream)
-    where   success img = 
+    where   success img =
                 do  putStrLn $ "image colour space: " ++ getColourSpaceName img
-                    let imgPixels = convertImageToDoubleList img
+
+                    let imgPixels = convertImageToVectors img
+                        g = kmeans id euclidSq 6 imgPixels
                         colours = buildTerminalColours imgPixels
-                    putStrLn $ show colours
-                    putStrLn $ show $ map rgbToHexCode colours
+                        coloursList = map V.toList colours
+
+                    putStrLn $ (++) "lengths: "
+                                $ show
+                                $ DV.map (\a -> length (elements a)) g
+
+                    putStrLn $ show coloursList
+                    putStrLn $ show $ map rgbToHexCode coloursList
             failure msg =
                 do  exitWithError $ "Error decoding image:\n" ++ msg
 
